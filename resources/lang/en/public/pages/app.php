bc9b570f0d5ec492de850ae0dac03d2227dfc2d3
David Ajnered <david@ajnered.com>
2018-05-25 20:45:45 +0200

    App page

---
+++ resources/lang/en/public/pages/app.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => 'App',
+    'header' => 'Local Food App',
+    'sub_header' => 'Coming soon to iOS and Android',
+    'content' => '
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis volutpat accumsan. Ut non justo in risus porttitor fermentum. In gravida massa diam, a varius sapien pulvinar sit amet. Donec ac justo quis mi posuere mollis et lacinia orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus malesuada semper velit. Donec vel congue odio. Quisque sodales tortor pulvinar urna ultrices vehicula. Mauris efficitur sagittis ligula, eu placerat turpis sollicitudin et.
+        </p>
+        <p>
+            Curabitur vel tortor eros. Morbi non semper felis. Cras sit amet egestas mauris. Quisque nec sagittis magna. Ut tempor ullamcorper risus, quis facilisis ipsum efficitur nec. Phasellus suscipit nec lectus consectetur molestie. Nulla cursus massa eu odio dapibus porta. Mauris luctus, magna vitae tempor tincidunt, nibh ipsum sollicitudin mauris, vel efficitur turpis massa in lacus. Mauris quis nunc leo. Donec egestas tincidunt eros, quis pellentesque neque pharetra ac. Suspendisse mattis risus pretium massa hendrerit, at feugiat mi tincidunt. Fusce sed nulla vel sapien tempus mattis in quis tortor.
+        </p>
+    ',
+];
+
