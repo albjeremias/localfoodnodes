1b371f5d876eb7b7b864990ce5e74ae8c303a092
frodeberg <alexander.frodeberg@gmail.com>
2018-06-05 19:26:42 +0200

    Responsive - Footer

--- resources/views/new/public/footer.blade.php
+++ resources/views/new/public/footer.blade.php
@@ -1,39 +1,35 @@
 @if(!isset($no_nav))
-<section class="container-fluid footer">
-    <div class="container pt-5 text-center">
-        <ul class="list-inline">
-            <li class="list-inline-item">
-                <a href="mailto:info@localfoodnodes.org">info@localfoodnodes.org</a>
-            </li>
+    <section class="container-fluid footer">
+        <div class="container pt-5">
 
-            <li class="list-inline-item">
-                <a href="#">+46 735 325945</a>
-            </li>
+            <div class="row d-flex">
+                <div class="col-16 col-sm-6 m-auto col-xl-11 text-xl-right">
+                    <a class="d-block d-xl-inline" href="mailto:info@localfoodnodes.org">info@localfoodnodes.org</a>
+                    <a class="d-block d-xl-inline" href="#">+46 735 325945</a>
+                    <a class="d-block d-xl-inline" href="#">Backavägen 8, 268 68 Röstånga</a>
+                    <a class="d-block d-xl-inline" href="#">Org.nr: 769633-6598</a>
+                </div>
 
-            <li class="list-inline-item">
-                <a href="#">Backavägen 8, 268 68 Röstånga</a>
-            </li>
+                <div class="d-flex col-16 order-last col-sm-4 order-sm-0 col-xl-16 order-xl-last mt-xl-5">
+                    <div class="ml-2 my-3 m-sm-auto">
+                        <img src="/images/nav-logo-dark.png">
+                    </div>
+                </div>
 
-            <li class="list-inline-item">
-                <a href="#">Org.nr: 769633-6598</a>
-            </li>
+                <div class="col-8 col-sm-6 m-sm-auto col-xl-5">
+                    <div class="text-sm-center text-xl-left">
+                        <p class="d-xl-inline-block mb-0 mb-3 mb-sm-0"><a href="/gdpr">GDPR</a></p>
 
-            <li class="list-inline-item">
-                <a href="/gdpr">GDPR</a>
-            </li>
+                        <a class="" href="https://www.facebook.com/localfoodnodes">
+                            <i class="icon black-54 fa fa-facebook-official" aria-hidden="true"></i>
+                        </a>
 
-            <li class="list-inline-item">
-                <a href="https://www.facebook.com/localfoodnodes"><i class="icon black-54 fa fa-facebook-official" aria-hidden="true"></i></a>
-            </li>
-
-            <li class="list-inline-item">
-                <a href="https://github.com/localfoodnodes"><i class="icon black-54 fa fa-git-square" aria-hidden="true"></i></a>
-            </li>
-        </ul>
-    </div>
-
-    <div class="d-flex justify-content-center mt-4">
-        <img src="/images/nav-logo-dark.png">
-    </div>
-</section>
+                        <a class="" href="https://github.com/localfoodnodes">
+                            <i class="icon black-54 fa fa-git-square" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endif
