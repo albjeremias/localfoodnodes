8964c4c4b121da0983ead6bd6ba89afcd3e06994
frodeberg <alexander.frodeberg@gmail.com>
2018-06-07 23:59:38 +0200

    Changed sub-nav icon & paddings

--- resources/views/new/layouts/nav/sub-menu.blade.php
+++ resources/views/new/layouts/nav/sub-menu.blade.php
@@ -9,7 +9,7 @@
     ];
 
     $account_navbar = [
-        ['name' => 'Dashboard',   'link' => '/account/user',  'icon' => 'shopping-basket'],
+        ['name' => 'Dashboard',   'link' => '/account/user',  'icon' => 'th-large'],
         ['name' => 'Mina Noder',  'link' => '/account/nodes', 'icon' => 'map-marker'],
         ['name' => 'Utlämningar', 'link' => '#',              'icon' => 'home'],
         ['name' => 'Evenemang',   'link' => '#',              'icon' => 'calendar'],
@@ -32,7 +32,7 @@
     <div class="bg-main-primary wc text-uppercase sub-nav text-center text-lg-left">
         <ul class="list-inline mt-2 d-flex d-md-block mb-0">
             @foreach($active_navbar as $nav_item)
-                <li class="list-inline-item flex-row w-25 {{ $sub_nav_active == $loop->index ? 'sub-nav-active' : '' }} pb-4 pb-lg-3">
+                <li class="list-inline-item flex-row w-25 {{ $sub_nav_active == $loop->index ? 'sub-nav-active' : '' }} pt-3 pt-md-0 pb-3 pb-md-4 pb-lg-3">
                     <a class="px-md-3 wc" href="{{ $nav_item['link'] }}">
                         <span class="d-none d-md-inline">{{ $nav_item['name'] }}</span>
                         <i class="fa fa-{{ $nav_item['icon'] }} d-md-none icon w-100" aria-hidden="true"></i>
