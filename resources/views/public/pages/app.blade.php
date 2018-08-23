b93ab67332ce4249256fa3c5acedc605e8506bc4
David Ajnered <david@ajnered.com>
2018-08-23 13:46:46 +0200

    Typo and app store logos

--- resources/views/public/pages/app.blade.php
+++ resources/views/public/pages/app.blade.php
@@ -12,12 +12,18 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body body-text">
-                        {!! trans('public/pages/app.content_top') !!}
+                        <div class="row mb-5 text-center text-lg-right">
+                            <div class="col-12">
+                                <a target="_blank" href="https://itunes.apple.com/us/app/local-food-app/id1400957388"><img src="/images/app-store-ios.png"></a>
+                                <a target="_blank" href="https://play.google.com/store/apps/details?id=org.localfoodnodes.localfoodapp"><img src="/images/app-store-android.png"></a>
+                            </div>
+                        </div>
                         <div class="text-center mb-5">
                             <img src="/images/app-map.png" style="height: 400px; width: auto;" />
                             <img src="/images/app-products.png" style="height: 400px; width: auto;" />
                             <img src="/images/app-orders.png" style="height: 400px; width: auto;" />
                         </div>
+                        {!! trans('public/pages/app.content_top') !!}
                         {!! trans('public/pages/app.content_bottom') !!}
                     </div>
                 </div>
