b551a1c140c9b6d32b64c15e72ee588ef5146327
David Ajnered <david@ajnered.com>
2018-09-04 12:38:33 +0200

    Team typo

--- resources/lang/en/public/pages/team.php
+++ resources/lang/en/public/pages/team.php
@@ -18,7 +18,7 @@ return [
     ',
     'davidajnered_title' => 'Backend web developer',
     'davidajnered' => '
-        <p>I\'m a webdeveloper comfortable working both back-end and front-end. I get a kick out of small fast projects that go from nothing to awesome in a few weeks but prefer to work in large complex systems where writing efficient, self explaining and maintainable code is important.</p>
+        <p>I\'m a web developer comfortable working both back-end and front-end. I get a kick out of small fast projects that go from nothing to awesome in a few weeks but prefer to work in large complex systems where writing efficient, self explaining and maintainable code is important.</p>
         <p>I dream about living on a farm producing my own food, minimizing the need for money and spending time being in the moment doing what I love.</p>
     ',
 ];
--- resources/lang/sv/public/pages/team.php
+++ resources/lang/sv/public/pages/team.php
@@ -17,7 +17,7 @@ return [
     ',
     'davidajnered_title' => 'Backend webbutvecklare',
     'davidajnered' => '
-        <p>Jag är en webbutvecklare som är bekräv med att jobba både frontend och backend. Jag får en kick av små snabba projekt som går från inget till något fantastiskt på några veckor, men jag föredrar att jobba i större komplexa system där optimerad, självförklarade och underhållbar kod är viktigt.</p>
+        <p>Jag är en webbutvecklare som är bekväm med att jobba både frontend och backend. Jag får en kick av små snabba projekt som går från inget till något fantastiskt på några veckor, men jag föredrar att jobba i större komplexa system där optimerad, självförklarade och underhållbar kod är viktigt.</p>
         <p>Jag drömmer om att bo på en mindre gård där jag producerar min egen mat och minimerar behover av pengar så jag kan spendera tid med att vara i nuet och göra saker jag älskar.</p>
     ',
 ];
