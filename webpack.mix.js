963c17ee4738d514cd8197b52f86d062fe5cc54a
David Ajnered <david@ajnered.com>
2019-02-11 05:54:12 +0100

    Correct path to js resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,19 +9,19 @@ mix.sass('resources/assets/sass/public.sass', 'public/css').version();
 mix.sass('resources/assets/sass/account.sass', 'public/css').version();
 
 // Vue
-mix.js('resources/assets/js/vue/payment-widget/payment-widget.js', 'public/js').version();
+mix.js('resources/assets/js/payment-widget/payment-widget.js', 'public/js').version();
 
 // Frontpage
-mix.js('resources/assets/js/vue/economy-in-out/economy-in-out.js', 'public/js').version();
-mix.js('resources/assets/js/vue/economy-circulation/economy-circulation.js', 'public/js').version();
+mix.js('resources/assets/js/economy-in-out/economy-in-out.js', 'public/js').version();
+mix.js('resources/assets/js/economy-circulation/economy-circulation.js', 'public/js').version();
 
-mix.js('resources/assets/js/vue/transactions/transactions.js', 'public/js').version();
-mix.js('resources/assets/js/vue/admin-transaction-list/admin-transaction-list.js', 'public/js').version();
+mix.js('resources/assets/js/transactions/transactions.js', 'public/js').version();
+mix.js('resources/assets/js/admin-transaction-list/admin-transaction-list.js', 'public/js').version();
 
-mix.js('resources/assets/js/vue/producer-map/producer-map.js', 'public/js').version();
-mix.js('resources/assets/js/vue/node-map/node-map.js', 'public/js').version();
+mix.js('resources/assets/js/producer-map/producer-map.js', 'public/js').version();
+mix.js('resources/assets/js/node-map/node-map.js', 'public/js').version();
 
 mix.js([
-    'resources/assets/js/vue/new/index.js',
+    'resources/assets/js/new/index.js',
     'node_modules/slick-carousel/slick/slick.min.js'
 ], 'public/js/new.js').version();
