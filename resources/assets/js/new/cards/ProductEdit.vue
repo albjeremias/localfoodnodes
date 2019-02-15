bd8c6352d91828ce37270359a902b3275464263b
Riggs1992 <alexander.frodeberg@gmail.com>
2019-02-15 15:04:54 +0100

    Alot of fixes

--- resources/assets/js/new/cards/ProductEdit.vue
+++ resources/assets/js/new/cards/ProductEdit.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="product product-edit">
-        <div class="product__img-container image bg-img-basket" style="">
-            <!--<span class="product-items-quantity badge badge-warning">SLUT</span>-->
-
-            <div class="transparent-circle-icon-bg">
-                <i class="product__img-container__share" aria-hidden="true"></i>
+        <div class="visibility-overlay">
+            <div class="m-auto">
+                <a :href="'/account/producer/' + producer.id + '/product/' + product.id"><h2 class="text-center mb-5 wc">{{ product.name }}</h2></a>
+                <button class="btn btn-lg btn-white bb px-5">Make available</button>
             </div>
+        </div>
 
+        <div class="product__img-container image bg-img-basket" style="">
             <small class="product__img-container__tag">{{ tag }}</small>
         </div>
 
         <div class="product-container px-3 py-1">
 
-            <a :href="'/account/producer/' + producer.id + '/product/' + product.id"><h4 class="rc h10 mt-1 line-clamp-1">{{ product.name }}</h4></a>
+            <a :href="'/account/producer/' + producer.id + '/product/' + product.id"><h4 class="rc h10 mt-1 line-clamp-1 d-inline-block">{{ product.name }}</h4></a>
+            <i class="fas fa-share-alt"></i>
 
             <select v-if="variants.length > 0" class="custom-select bb-38">
                 <option v-for="variant in variants" :key="variant.id">
@@ -21,26 +23,25 @@
             </select>
 
             <div v-else class="divider d-flex">
-                <small class="font-italic my-auto">This product doesn't have any variants</small>
+                <select class="w-100"></select>
             </div>
 
             <div class="form-container">
                 <hr class="mb-2">
-                <p class="mb-2 rc d-inline-block">Delivery date settings</p>
+
+
+
+                <p class="mb-2 d-inline-block">Delivery date settings</p>
                 <info :text="info.settings.text" :placement="info.settings.placement" :icon-class="info.settings.class"></info>
 
                 <div class="product-edit-form">
-                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group w-25" id="form-input-stock" value="" placeholder="Stock" autocomplete="off">
-                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group w-25 mx-lg-1" id="form-input-price" value="" placeholder="Price" autocomplete="off">
-                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group w-25 mr-lg-1" id="form-input-deadline" value="" placeholder="Deadline" autocomplete="off">
-                    <a class="btn btn-secondary wc" @click="saveSettings()">Save</a>
+                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group" id="form-input-stock" value="" placeholder="Stock" autocomplete="off">
+                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group mx-lg-1" id="form-input-price" value="" placeholder="Price" autocomplete="off">
+                    <input type="text" aria-describedby="input-aria-name" name="name" class="form-control input-group mr-lg-1" id="form-input-deadline" value="" placeholder="Deadline" autocomplete="off">
                 </div>
-            </div>
+                <a class="btn btn-secondary wc mt-2 w-100" @click="saveSettings()">Save changes</a>
 
-            <div class="custom-control custom-checkbox custom-checkbox-lg product-edit-active">
-                <input type="checkbox" :checked="product.is_hidden" @click="toggleProductVisibility(product.id)" :name="'checkbox-active-' + product.id" class="custom-control-input" :id="'checkbox-active-' + product.id">
-                <label class="custom-control-label" :for="'checkbox-active-' + product.id">Make available</label>
-                <info :text="info.available.text" :placement="info.available.placement" :icon-class="info.available.class"></info>
+                <i class="fas fa-unlock-alt"></i>
             </div>
         </div>
     </div>
--- resources/views/new/account/product/overview/index.blade.php
+++ resources/views/new/account/product/overview/index.blade.php
@@ -1,6 +1,7 @@
-@extends('new.account.layout',
+    @extends('new.account.layout',
 [
-    'nav_title'      => trans('admin/user.nav_title'),
+    'bread_type'     => __('Product'),
+    'bread_result'   => $product->name,
     'sub_nav'        => 'producer',
     'sub_nav_active' => 1,
     'nav_active'     => 1
@@ -18,29 +19,79 @@
             @endphp
         @endforeach
 
-        <div class="container py-5">
+        <div class="image product-background" style="background-image: url('{{ $product->images()[0]->url('large') }}')"></div>
+
+        <div class="product-info container wc nm">
             <div class="row">
                 <div class="col-16">
-                    <div class="white-box">
-                        <h4 class="rc text-center mb-3">{{ $product->name }}</h4>
-                        <p>
-                            På denna sida kan du göra flera och mer detaljerade inställningar som passar för denna produkt.
-                            Du når alltid denna vy närsomhelst för varje enskild produkt i din produktvy.
-                        </p>
-                        <div class="d-flex">
-                            <a class="btn btn-primary mx-auto" href="{{ '/account/producer/' . $active_producer_id . '/products' }}">{{ __('Back to products') }}</a>
+                    <h1 class="d-inline-block">{{ $product->name }}</h1>
+
+                    <ul class="list-inline pt-2">
+                        <li class="list-inline-item mr-4">
+                            <a class="btn btn-secondary box-shadow" href="#">{{ __('View product') }}</a>
+                        </li>
+
+                        <li class="list-inline-item mr-4 btn btn-white box-shadow">
+                                <i class="fa fa-share-alt mr-2" aria-hidden="true"></i>
+                                Share product
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <section id="overview">
+            <div class="container">
+                <div class="row mt-4">
+                    <div class="col-16 col-xl-10">
+                        <div class="overview-box">
+                            <h4>{{ __('Overview') }}</h4>
+                            {{ __('Here you can make more detailed customizations to best suit this specific product.
+                                You can always reach this view at any time from your product administration view.') }}
+
+                            <a class="bottom-left-link" href="{{ route('account_producer_products', ['producerId' => $producer->id]) }}">
+                                {{ __('Back to products') }}
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-16 col-xl-6 mt-5 mt-xl-0">
+                        <div class="product-stats">
+                            <h4 class="mb-0">{{ __('Product stats') }}</h4>
+
+                            <div class="row mt-3">
+                                <div class="col-16 col-sm-4 col-lg-3 col-xl-4">
+                                    <h4 class="mb-0">1129</h4>
+                                    <small>{{ __('Sold') }}</small>
+                                </div>
+
+                                <div class="col-16 col-sm-4 col-lg-3 col-xl-4">
+                                    <h4 class="mb-0">41</h4>
+                                    <small>{{ __('Pending') }}</small>
+                                </div>
+
+                                <div class="col-16 col-sm-8 col-lg-3 col-xl-8">
+                                    <h4 class="mb-0">13.5 km</h4>
+                                    <small>{{ __('Revenue') }}</small>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                @include('new.account.product.overview.basic-info')
-
-                @include('new.account.product.overview.stocks-and-variants')
-
-                @include('new.account.product.overview.delivery-dates')
-
-                @include('new.account.product.overview.meat-box')
             </div>
+        </section>
+
+
+        <div class="container">
+            @include('new.account.product.overview.basic-info')
+
+            @include('new.account.product.overview.stocks-and-variants')
+
+            @include('new.account.product.overview.delivery-dates')
+
+            @include('new.account.product.overview.meat-box')
+        </div>
+    </div>
 
         </div>
     </div>
